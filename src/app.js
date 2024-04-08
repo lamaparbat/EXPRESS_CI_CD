780aac306c402c410d7c8be097fe4c7b4254b74f
Parbat Lama <lamaparbat70@gmail.com>
2024-04-08 13:24:17 +0545

    Feat: Test commit action yml

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ startMongoConnection().then(() => {
     app.use(express.json());
 
     app.get('/', (req, res) => {
-        res.send("Hello From LamaDev !!")
+        res.send("Hello From LamaDev 123 !!")
     });
 
     app.listen(port, () => console.log(`Server listening on ${port}`));
