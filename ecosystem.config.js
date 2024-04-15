1fcf87ba1b44cfb952bebbc6b38da201a6dcee5c
Parbat Lama <lamaparbat70@gmail.com>
2024-04-15 09:39:55 +0545

    Chore: PM2 Configuration done

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps: [{
+    name: "app",
+    script: './src/app.js',
+    ignore_watch: ["node_modules", "\\.git"]
+  }],
+};
--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ startMongoConnection().then(() => {
     app.use(express.json());
 
     app.get('/', (req, res) => {
-        res.send("Hello From LamaDev. yooooo wassup bro !!")
+        res.send("Hello From LamaDev. 123 wassup bro !!")
     });
 
     app.listen(port, () => console.log(`Server listening on ${port}.`));
